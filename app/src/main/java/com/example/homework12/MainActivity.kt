e631deb6fd4684e33c344334d903e3e28a9d8c90
s-smolenkov <smolenkovserhii@gmail.com>
2024-01-13 01:45:53 +0200

    Finished homework

--- app/src/main/java/com/example/homework12/MainActivity.kt
+++ app/src/main/java/com/example/homework12/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.homework12
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.Toast
 
@@ -11,12 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val flSendButton : FrameLayout = findViewById(R.id.flSendButton)
+        val emailFiled : EditText = findViewById(R.id.email)
+        val subjectFiled : EditText = findViewById(R.id.subject)
+        val messageFiled : EditText = findViewById(R.id.message)
+
         flSendButton.setOnClickListener{
-            Toast.makeText(
-                this,
-                "sending...",
-                Toast.LENGTH_LONG
-            ).show()
+            val email = emailFiled.text.toString()
+            val subject = subjectFiled.text.toString()
+            val message = messageFiled.text.toString()
+            sendEmail(email, subject, message)
         }
+    }
+
+    private fun sendEmail(email: String, subject: String, message: String){
+        val intent = Intent(Intent.ACTION_SEND).apply {
+//            type = "message/rfc822"
+            type = "text/plain"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+        startActivity(Intent.createChooser(intent, "Choose an Email client"))
     }
 }
